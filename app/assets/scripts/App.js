831f09b52db99255d8a55b968f79807bde668835
codessbabe <oana.anghelachi@gmail.com>
2020-04-21 11:34:24 +0200

    Build "Modal/Lightbox Overlay"

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu' 
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
-
+new Modal();
 let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 
